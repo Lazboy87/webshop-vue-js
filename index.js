40cdf0baa3129e20af9b6f650092858836e4fa7d
LASSE <lassehovden@gmail.com>
2019-10-04 02:34:05 +0200

    adding files to repo simple webshop

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+
+
+var app= new Vue({
+
+
+    el: "#app",
+    
+    data:{
+        maximum:300,
+        products: null,
+        cart:[],
+
+    },
+    methods: {
+        addItem: function(product){
+            this.cart.push(product);
+            
+        },
+    },
+    
+    mounted:function(){
+
+        fetch('https://hplussport.com/api/products/order/price')
+        .then(response => response.json())
+        .then(data =>{
+            this.products = data;
+        })
+    }
+   
+   
+            
+    
+    
+     });
+     
